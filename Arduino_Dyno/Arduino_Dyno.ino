93c8ebc8af006d252a6cc25178687f6d83503a0a
Mpc1055 <michael.caiozzo@gmail.com>
2025-01-02 16:56:08 -0500

    Intial with ISR and LN298

--- Arduino_Dyno/Arduino_Dyno.ino
+++ Arduino_Dyno/Arduino_Dyno.ino
@@ -1,12 +1,62 @@
-//Dyometer Code for Tony B
-//Written by Michael Caiozzo 
+/*
+Dynometer Code for Tony B
+Written by Michael Caiozzo
+
+Note Optical Sensor must be hooked up
+to Pin 2
+
+*/
+
+#define opticalSensor 2
+#define LED 13
+
+int  was        = 0; // Used to store or "now time"
+int  counter    = 0; // Store number of beam breaks
+int  RPS        = 0; // Revolutions Perr Second
+int  LED_State  = 0; // Used to Toggle Led ON and off
+
+//LN298 CODE
+#define ENA 8
+#define IN1 9
+#define IN2 10
 
 void setup() {
-  // put your setup code here, to run once:
+  
+  pinMode(opticalSensor, INPUT);// Set optical Sensor to INPUT 
+  pinMode(LED, OUTPUT);         // Set LED to OUTPUT
+  Serial.begin(9600);           //Intialize Serial Monitor
+
+  // Activate Pin 2's interupt to call ISR1 function on the Rising edge of signal
+  attachInterrupt(digitalPinToInterrupt(opticalSensor), ISR1, RISING);
+ 
+  //LN298 Code Setup
+  pinMode(ENA, OUTPUT);
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+
+  //LN298 Code Turn Motor On, Spin foward
+  digitalWrite(ENA, HIGH);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  was = millis();           // Set was to current time
+  LED_State = ~LED_State;   // Invert State of LED
+
+while( (millis() - was) < 1000){
+  digitalWrite(LED, LED_State); // Toggle LED on and off based on LED_State
+} 
+
+ RPS = counter/2;             // Calculate RPS by dividing toal # of beam breaks by number of slits in wheel
+ Serial.println(RPS);         // Print RPS
+ counter = 0;                 // Reset counter
 
 }
+
+//Interupt Service Routine Function
+void ISR1 (){
+  counter +=1; //Increment counter
+}
+
