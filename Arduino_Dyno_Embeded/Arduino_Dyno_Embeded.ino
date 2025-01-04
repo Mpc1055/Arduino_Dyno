e3a24462bf336da804b07b8baf79c683d035e92a
Mpc1055 <michael.caiozzo@gmail.com>
2025-01-04 15:18:42 -0500

    started embedded version

--- Arduino_Dyno_Embeded/Arduino_Dyno_Embeded.ino
+++ Arduino_Dyno_Embeded/Arduino_Dyno_Embeded.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+int counter = 0;
 
+void setup() {
+  
+  Serial.begin(9600);
+ 
+  TCCR1A = 0;
+  TCCR1B = 4;
+  TCNT1 = 3036;   
+  TIMSK1 = 1;
+  interrupts();
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+}
+
+
+ISR(TIMER1_OVF_vect){//______ISR for when timer1A interrupts
+
+  TCNT1 = 3036;   
+  Serial.println("1 second");
 
 }
