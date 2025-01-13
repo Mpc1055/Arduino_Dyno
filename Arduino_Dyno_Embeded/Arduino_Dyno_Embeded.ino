b027ca9358ec6b913446e867983ba80849ba7e62
Mpc1055 <105609966+Mpc1055@users.noreply.github.com>
2025-01-12 20:51:21 -0500

    Update Arduino_Dyno_Embeded.ino
    renamed variables

--- Arduino_Dyno_Embeded/Arduino_Dyno_Embeded.ino
+++ Arduino_Dyno_Embeded/Arduino_Dyno_Embeded.ino
@@ -9,8 +9,8 @@
 float counter = 0;
 float rps = 0;
 
-long TCNt1_value = 0;
-int TCCR1B_value = 0;
+long TCNT1_value = 0;
+int  TCCR1B_value = 0;
 
 void setup() {
 
@@ -29,12 +29,12 @@ void setup() {
   Serial.begin(9600);
 
   //calculate timer 1 values (tcnt1 & tccR1b)
-  timer1_intialize(250 * pow(10,-3));
+  timer1_init(250 * pow(10,-3));
 
   //Timer 1 Setup
   TCCR1A = 0;
   TCCR1B = TCCR1B_value;
-  TCNT1 = TCNt1_value;   
+  TCNT1 = TCNT1_value;   
   TIMSK1 = 1;
   interrupts();
 
@@ -46,7 +46,7 @@ void loop() {
 
 //Timmer Overflow
 ISR(TIMER1_OVF_vect){//______ISR for when timer1A interrupts
-  TCNT1 = TCNt1_value; 
+  TCNT1 = TCNT1_value; 
   rps = (counter/2) *20;
   counter = 0;
   Serial.println(rps);
@@ -56,34 +56,34 @@ void ISR1 (){
   counter +=1; //Increment counter
 }
 
-void timer1_intialize(float period){
+void timer1_init(float period){
   double clk = 1.0/16000000;
 
     // No Prescale
   if ( (period / clk ) < 65535 ){
     TCCR1B_value = 1;
-    TCNt1_value = 65535 - (period / clk);
+    TCNT1_value = 65535 - (period / clk);
     }
     //8 Prescale
     else if ((period / (clk* 8) ) < 65535 ){
       TCCR1B_value = 2;
-      TCNt1_value = 65535 - (period / (clk* 8));
+      TCNT1_value = 65535 - (period / (clk* 8));
     } 
 
     //64 Prescale
     else if ((period / (clk* 64) )< 65535 ){
       TCCR1B_value = 3;
-      TCNt1_value = 65535- (period / (clk* 64));
+      TCNT1_value = 65535- (period / (clk* 64));
     } 
     // 256 Prescale
     else if ((period / (clk* 256) )< 65535 ){
       TCCR1B_value = 4;
-      TCNt1_value = (pow(2,16)-1) - (long(period / (clk* 256)));
+      TCNT1_value = (pow(2,16)-1) - (long(period / (clk* 256)));
     }
     // 1024 Prescale 
     else if ( (period / (clk* 1024)) < 65535 ){
       TCCR1B_value = 5;
-      TCNt1_value = 65535 - (period / (clk* 1024));
+      TCNT1_value = 65535 - (period / (clk* 1024));
     }  
     else {
       Serial.println("Invalid Period Selected");
